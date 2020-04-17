fe3b941b3b35a5fe18dcaaaa84232b420a8c9bf9
vostrikov2014 <vostrikov2014@gmail.com>
2020-04-17 18:36:21 +0300

    Урок 2, задание 1

---
+++ leeson_2/ex_1.rb
@@ -0,0 +1,2 @@
+day_month = {january: 31, february: 28, march: 31, april: 30, may: 31, june: 30, july: 31, august: 31, september: 30, october: 31, november: 30, december: 31}
+day_month.each { |key, value| puts value if value == 30 }
