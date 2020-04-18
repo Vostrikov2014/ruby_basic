752c794c7b77b01325ab4d42c63fd33877459a73
vostrikov2014 <vostrikov2014@gmail.com>
2020-04-18 14:30:19 +0300

    задача 4

--- leeson_2/ex_4.rb
+++ leeson_2/ex_4.rb
@@ -3,18 +3,17 @@
 =end
 
 vowels_hash = {}
-
-alphabet = ('a'..'z')
 vowels = ['a', 'e', 'i', 'o', 'u']
 
 =begin
 еще возможн так...  как обычно объявляют массив в такой ситуации?
 vowels = %w[a e i o u]
 vowels = 'aeiou'.chars
+
+можно так же объявить диапазон но опять же хз как лучше?
+alphabet = ('a'..'z')
 =end
 
-alphabet.each.with_index() do |char, i|
-  vowels_hash[char] = i if vowels.include?(char)
-end
+('a'..'z').each.with_index() { |char, i| vowels_hash[char] = i if vowels.include?(char) }
 
 puts vowels_hash
