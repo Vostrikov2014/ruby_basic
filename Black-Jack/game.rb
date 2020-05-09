c176a1d0b3bc39d905fd2909f9fdce9bf72608d3
vostrikov2014 <vostrikov2014@gmail.com>
2020-05-09 17:57:49 +0300

    Black Jack geme

--- Black-Jack/game.rb
+++ Black-Jack/game.rb
@@ -9,7 +9,7 @@ class Game
     @user = User.new(name)
     @dealer = Dealer.new('Dealer')
     @card_index = 0
-    @round_bank = 0
+    @bank_game = 0
   end
 
   def start_game
@@ -30,8 +30,8 @@ class Game
     @card_deck = CardDeck.new
     @card_deck_shuffled = @card_deck.shuffled_card
 
-    @round_bank = 0
-    @round_bank += REGULAR_BANK * 2
+    @bank_game = 0
+    @bank_game += REGULAR_BANK * 2
 
     @user.cards = []
     @user.score = 0
@@ -64,5 +64,19 @@ class Game
     user.cards << @card_deck_shuffled[@card_index]
     user.score += @card_deck.card_value(@card_deck_shuffled[@card_index])
     @card_index += 1
+  end
+
+  def open_cards
+    if @user.score > @dealer.score && @user.score <= 21 || @dealer.score > 21 && user.score <= 21
+      @user.get_win(@bank_game)
+      puts "Победил #{@user.name}!"
+    elsif @dealer.score > @user.score && @dealer.score <= 21 || @dealer.score <= 21 && user.score > 21
+      @dealer.get_win(@bank_game)
+      puts "Победил #{@user.name}!"
+    else
+      @user.get_win(@bank_game / 2)
+      @dealer.get_win(@bank_game / 2)
+      puts 'Ничья'
+    end
   end
 end
