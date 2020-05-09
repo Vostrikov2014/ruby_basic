24085afdde0bd18a6b0729b2c55c571e91a4f241
vostrikov2014 <vostrikov2014@gmail.com>
2020-05-09 16:17:27 +0300

    Black Jack geme

--- Black-Jack/game.rb
+++ Black-Jack/game.rb
@@ -15,45 +15,45 @@ class Game
   def start_game
     loop do
       new_game
-      user_make_game(@user)
-      dealer_make_game(@dealer)
+      user_make_game
+      dealer_make_game
     end
   end
 
   def new_game
     @card_deck = CardDeck.new
     @card_deck_shuffled = @card_deck.shuffled_card
-    @user.cards = []
-    @dealer.cards = []
-    @user.score = 0
-    @dealer.score = 0
+
     @round_bank = 0
     @card_index = 0
 
-    @user.cards << @card_deck[@card_index, @card_index += 1]
+    @user.cards = []
+    @user.score = 0
+    (1..2).each { |n| add_card(@user)}
     @user.put_bank(REGULAR_BANK)
     puts @user.cards
 
-    @dealer.cards << @card_deck[@card_index += 1, @card_index += 1]
+    @dealer.cards = []
+    @dealer.score = 0
+    (1..2).each { |n| add_card(@dealer)}
     @dealer.put_bank(REGULAR_BANK)
 
     @round_bank += REGULAR_BANK * 2
   end
 
-  def user_make_game(user)
+  def user_make_game
     puts "Карты: #{@user.cards}"
     puts "Очки: #{@user.score}"
     puts ' Введите: 1 - пропустить ход, 2 - добавить карту, 3 - открыть карты'
     entered_number = gets.chomp.to_i
 
     return if entered_number == 1
-    add_card(user) if entered_number == 2
+    add_card(@user) if entered_number == 2
     open_cards if entered_number == 3
   end
 
-  def dealer_make_game(user)
-
-
+  def dealer_make_game
+    add_card(@dealer) if @dealer.score < 17
   end
 
   def add_card(user)
